a898a62f77ba4d81888b656847dea7678c796d0b
Harisanth P S <83811783+harisanth123@users.noreply.github.com>
2021-06-18 16:49:59 +0530

    speed and voice
    checking  the voice and speed of the voice

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ import pyttsx3 as p
 #instance to get driver of computer
 engine = p.init()
 
-engine.say(" my name is sofia.")
+#speed of speeking
+rate = engine.getProperty('rate')
+engine.setProperty('rate',180)
+print(rate)
+
+#checking voice
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[0].id)
+print(voices)
+
+engine.say(" my name is sofia. your voice assistant")
 
 #wait till output is finish
 engine.runAndWait()#c
