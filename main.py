59a20007dc2c6c663696ddd6a800fcf31e4900e9
Harisanth P S <83811783+harisanth123@users.noreply.github.com>
2021-06-18 17:13:52 +0530

    listen to voice
    connect to google api for voice to text recognition

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 #libraries
 import pyttsx3 as p
+import speech_recognition as sr
 
-#instance to get driver of computer
-engine = p.init()
 
-#speed of speeking
-rate = engine.getProperty('rate')
+engine = p.init()#---------------------------------------------------instance to get driver of computer
+
+
+rate = engine.getProperty('rate')# ----------------------------------speed of voice
 engine.setProperty('rate',180)
-print(rate)
 
-#checking voice
-voices = engine.getProperty('voices')
+voices = engine.getProperty('voices')# ------------------------------to check voice avilable
 engine.setProperty('voice',voices[0].id)
-print(voices)
 
 engine.say(" my name is sofia. your voice assistant")
+engine.runAndWait()# ------------------------------------------------to wait till voice complete
 
-#wait till output is finish
-engine.runAndWait()#c
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    r.energy_threshold=10000#----------------------------------------to get low voice
+    r.adjust_for_ambient_noise(source,1.2)#--------------------------cancel noice
+    print("listening")
+    audio = r.listen(source)#----------------------------------------listen to audio 
+    text = r.recognize_google(audio)#--------------------------------connect to google api
+    print(text)
